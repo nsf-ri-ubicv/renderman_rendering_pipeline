ec00d6e5205f94ce119992461a9b434e37fff800
Dan Yamins <dyamins@gmail.com>
2011-04-26 16:42:00 -0400

    still testing environment

--- renderer.py
+++ renderer.py
@@ -230,6 +230,7 @@ def init_job_template(jt,out_dir,bg_id,model_params,kenv,bg_phi,bg_psi):
     jt.remoteCommand = 'python'
     jt.workingDirectory = os.getcwd()
 
+    os.environ['PROCESSORS']='2' 
     argstr = "import renderer as R; R.render_single_image_queue(" + repr(out_dir) + "," + repr(bg_id) + "," + repr(model_params) + ", kenv=" + repr(kenv) + ", bg_phi=" + repr(bg_phi) + ", bg_psi=" + repr(bg_psi) + ")"
     jt.args = ["-c",argstr]
     jt.joinFiles = True
@@ -242,7 +243,8 @@ def init_job_template(jt,out_dir,bg_id,model_params,kenv,bg_phi,bg_psi):
                                                           'INFOPATH',
                                                           'MAYA_PLUG_IN_PATH',
                                                           'DL_DISPLAYS_PATH',
-                                                          'DL_SHADERS_PATH']])
-    jt.jobEnvironment['PROCESSORS']=2
+                                                          'DL_SHADERS_PATH',
+                                                          'PROCESSORS']])
+    
 
     return jt
