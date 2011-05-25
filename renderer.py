7b3b68843f4556d1949069dd30068a8405fe2d94
Dan Yamins <dyamins@gmail.com>
2011-05-25 15:19:15 -0400

    testing

--- renderer.py
+++ renderer.py
@@ -64,6 +64,7 @@ def mtl_fixer(path,model_id,libpath):
         oldpath = os.path.join(libpath,base + ext)
         ext = ext.lower()
         newpath = os.path.join(libpath,base + ext)
+        print(base,ext,oldpath,newpath)
         F = F.replace(d, ' ' + newpath)
         if oldpath != newpath:
             os.system('mv ' + oldpath + ' ' + newpath)
