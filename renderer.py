8fa82589eb3d4d2d4f32d88dd20909e3c5259860
Dan Yamins <dyamins@gmail.com>
2011-05-25 15:33:11 -0400

    bugfix testing

--- renderer.py
+++ renderer.py
@@ -68,7 +68,7 @@ def mtl_fixer(path,model_id,libpath):
         F = F.replace(d, ' ' + newpath)
         [dir,fname] = os.path.split(newpath)
         L = os.listdir(dir)
-        print(op,newpath)
+        print(newpath)
         op = [os.path.join(dir,l) for l in L if l.lower() == fname][0]
         if op != newpath:
             os.system('mv ' + op + ' ' + newpath)
