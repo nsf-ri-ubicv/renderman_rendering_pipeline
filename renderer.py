a470c57b74b4750cb1e576bf132eea28f45db9d1
Dan Yamins <dyamins@gmail.com>
2011-05-25 21:53:00 -0400

    bugifx

--- renderer.py
+++ renderer.py
@@ -70,7 +70,8 @@ def mtl_fixer(path,model_id,libpath):
         base = base.lower() ; ext = ext.lower()
         newpath = os.path.join(libpath,base + ext)
         print(libpath,base,ext)
-        F = F.replace(d, ' ' + newpath)
+        F = F.replace(d, base + ext)
+        print(F)
         [dir,fname] = os.path.split(newpath)
         L = os.listdir(dir)
         op = [os.path.join(dir,l) for l in L if l.lower() == fname][0]
